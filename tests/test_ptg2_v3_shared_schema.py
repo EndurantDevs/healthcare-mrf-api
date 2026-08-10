48a07f8c75f67240d65b13544ebe77ec3d0d1bcc
Dmytro Nikolayev <dnikolayev@gmail.com>
2026-08-10 09:01:54 +0200

    test(migrations): track enrichment publication head

--- tests/test_ptg2_v3_shared_schema.py
+++ tests/test_ptg2_v3_shared_schema.py
@@ -153,7 +153,7 @@ def test_repository_has_single_alembic_head():
     config = Config(str(root / "alembic.ini"))
 
     assert ScriptDirectory.from_config(config).get_heads() == [
-        "20260810050000_provider_directory_uhc_flex_npi_cohort"
+        "20260810080000_provider_directory_uhc_flex_practitioner_publication"
     ]
 
 
