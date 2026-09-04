27fe28af3da64402c2c10052d67f4e43b2ece30d
Dmytro Nikolayev <dnikolayev@gmail.com>
2026-09-04 16:56:18 +0200

    test: advance expected migration head

--- tests/test_ptg2_v3_shared_schema.py
+++ tests/test_ptg2_v3_shared_schema.py
@@ -153,7 +153,7 @@ def test_repository_has_single_alembic_head():
     config = Config(str(root / "alembic.ini"))
 
     assert ScriptDirectory.from_config(config).get_heads() == [
-        "20260903160000_plan_pricing_state_scan"
+        "20260904163000_provider_directory_exact_guard_scope"
     ]
 
 
